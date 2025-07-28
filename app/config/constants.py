9eedcd1288bb750501f12f9c279996785e1a8d89
LoversCode <jainmukti155@gmail.com>
2025-07-29 00:14:54 +0530

    Create constants.py
    Add Constant

---
+++ app/config/constants.py
@@ -0,0 +1,4 @@
+PRINTFUL_BASE_URL = "https://api.printful.com"
+PRINTFUL_UPLOAD_URL = f"{PRINTFUL_BASE_URL}/files"
+PRINTFUL_MOCKUP_TASK_URL = f"{PRINTFUL_BASE_URL}/mockup-generator/create-task/71"
+PRINTFUL_MOCKUP_STATUS_URL = f"{PRINTFUL_BASE_URL}/mockup-generator/task"
